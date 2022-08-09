a5ef84460e430a2c12e752dc9ad4d8d4e296bf14
javarip <kane.swartz@myport.ac.uk>
2022-08-09 12:26:24 +0100

    GOL node working

--- game-of-life/node/script.mjs
+++ game-of-life/node/script.mjs
@@ -1,40 +1,90 @@
 import fs from "fs";
 
-function main() {
-  const input = fs.readFileSync("../threeByThree.txt", "utf-8");
-  const cells = input.replaceAll("\n", "");
+async function main() {
+  const input = fs.readFileSync("../glider.txt", "utf-8");
+  const startState= input.replaceAll("\n", "");
   const numRows = getRows(input);
   const numCols = getCols(input);
   const numCells = numCols * numRows;
 
-  let outStr = '';
+  let state = startState;
+  for (let i = 0; i < 1000; i += 1) {
+    console.clear();
+    console.log(formatState(state, numCols));
 
-  for (let i = 0; i < cells.length; i += 1) {
-    if (i % numCols === 0) outStr += "\n";
-    outStr += `${cells[i]}:${getNumNeighbours(cells, i, numCols)}`;
+    let nextState = '';
+
+    for (let j = 0; j < state.length; j += 1) {
+      const numNeighbours = getNumNeighbours(state, j, numCols);
+
+      nextState += getNextCell(state[j], numNeighbours);
+    }
+    state = nextState;
+    await sleep(100);
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function getNextCell(cell, numNeighbours) {
+  if (cell === 'X') {
+    if (numNeighbours >= 2 && numNeighbours <= 3) return 'X';
+    else return '-';
+  } else {
+    if (numNeighbours === 3) return 'X';
+    else return '-';
+  }
+}
+
+function formatState(state, numCols) {
+  let retVal = '';
+  for (let i = 0; i < state.length; i += 1) {
+    if (i % numCols === 0) retVal += "\n";
+    retVal += state[i];
   }
 
-  console.log(outStr);
+  return retVal;
 }
 
 function getNumNeighbours(cells, index, numCols) {
   let neighbourCount = 0;
+
   // top
-  if (cells[index - numCols] === 'X') neighbourCount += 1;
+  if (cells[index - numCols] === 'X') {
+    neighbourCount += 1;
+  }
   // top right
-  if (cells[index - numCols + 1] === 'X') neighbourCount +=1;
+  if ((index + 1) % numCols !== 0 && cells[index - numCols + 1] === 'X') {
+    neighbourCount +=1;
+  }
   // right
-  if (cells[index + 1] === 'X') neighbourCount += 1;
+  if ((index + 1) % numCols !== 0 && cells[index + 1] === 'X') {
+    neighbourCount += 1;
+  }
   // bottom right
-  if (cells[index + numCols + 1] === 'X') neighbourCount += 1;
+  if ((index + 1) % numCols !== 0 && cells[index + numCols + 1] === 'X') {
+    neighbourCount += 1;
+  }
   // bottom
-  if (cells[index + numCols] === 'X') neighbourCount += 1;
+  if (cells[index + numCols] === 'X') {
+    neighbourCount += 1;
+  }
   // bottom left
-  if (cells[index + numCols - 1] === 'X') neighbourCount += 1;
+  if (index % numCols !== 0 && cells[index + numCols - 1] === 'X') {
+    neighbourCount += 1;
+  }
   // left
-  if (cells[index - 1] === 'X') neighbourCount += 1;
+  if (index % numCols !== 0 && cells[index - 1] === 'X') {
+    neighbourCount += 1;
+  }
   // top left
-  if (cells[index - numCols - 1] === 'X') neighbourCount += 1;
+  if (index % numCols !== 0 && cells[index - numCols - 1] === 'X') {
+    neighbourCount += 1;
+  }
 
   return neighbourCount;
 }
