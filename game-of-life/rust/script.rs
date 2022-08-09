012b055aac75b4621e96975e19db16c0e1815eed
javarip <kane.swartz@myport.ac.uk>
2022-08-09 23:15:12 +0100

    get cols & rows rust GOL

--- game-of-life/rust/script.rs
+++ game-of-life/rust/script.rs
@@ -3,12 +3,32 @@ use std::fs;
 fn main() {
     let filename = "../threeByThreeOnes.txt";
     let input = get_input_file(String::from(filename));
-    println!("{}", input);
+
+    let start = input.replace("\n", "");
+
+    let rows = get_rows(String::from(&input));
+    let cols = get_cols(String::from(&input));
+
+    println!("rows {}", rows);
+    println!("cols {}", cols);
+
+    println!("{}", start);
 }
 
-fn get_input_file(input: String) -> String {
-    let contents = fs::read_to_string(input)
-        .expect("Something went wrong reading the file");
+fn get_cols(input: String) -> usize {
+    input.split("\n").collect::<Vec<&str>>()[0].len()
+}
 
-    contents
+fn get_rows(input: String) -> usize {
+    input.split("\n").collect::<Vec<&str>>().len() - 1
+}
+
+// fn num_neighbours() -> i32 {
+    // let mut neighbour_count = 0;
+    // return neighbour_count;
+// }
+
+fn get_input_file(input: String) -> String {
+    fs::read_to_string(input)
+        .expect("Something went wrong reading the file")
 }
